2c866fed6fb3dc481d40b93bbd1c09f4ca65470c
Rafaela <rafaela.mumbach@acad.ufsm.br>
2024-04-03 17:54:46 -0300

    ok

--- Programacao de Computadores I/TDE01/exerc14.c
+++ Programacao de Computadores I/TDE01/exerc14.c
@@ -8,9 +8,27 @@
 
 int main() {
     int numFuncionario;
-    float salarioAtual, valorINT, indiceProd;
+    float salarioAtual, valorINT, indiceProd, aumento, novoSalario;
 
-    
+    printf("/* -- Calculo de Novo Salario! -- */\n");
+    printf("Numero do VENDEDOR: ");
+    scanf("%d", &numFuncionario);
+
+    printf("Salario ATUAL (R$): ");
+    scanf("%f", &salarioAtual);
+
+    printf("Informe o total do INTEMP (R$): ");
+    scanf("%f", &valorINT);
+
+    printf("Informe o indice de produtividade (R$): ");
+    scanf("%f", &valorINT);
+
+    aumento = 0.8 * valorINT + indiceProd;
+    novoSalario = salarioAtual + (salarioAtual * aumento);
+
+    printf("\nNumero do funcionario: %d\n", numFuncionario);
+    printf("Aumento salarial: %.2f%%\n", aumento * 100);
+    printf("Novo salario: R$ %.2f\n", novoSalario);
 
     return 0;
 }
