5ff8b9c79321fb2355f75f98407dd5f6fe196dfd
Rafaela <rafaela.mumbach@acad.ufsm.br>
2024-04-04 16:39:22 -0300

    ok

--- Programacao de Computadores I/TDE01/q1.c
+++ Programacao de Computadores I/TDE01/q1.c
@@ -1,13 +1,19 @@
-
+/* ---- TDE 01
+   ---- (Q1) Provas Anteriores     
+   ---- Rafaela Mumbach Buenos
+   ---- V03 - 04/04/2024
+*/
 
 #include <stdio.h>
 
 int main() {
+
     int hora_entrada, min_entrada, seg_entrada;
     int hora_saida, min_saida, seg_saida;
     int tempo_permanencia_seg;
+    int horaPermanencia = 0;
     float valor_compras;
-    float custo_estacionamento;
+    float valorEstacionamento;
     float desconto = 0.0;
 
     printf("ESTACIONAMENTO\n");
@@ -21,21 +27,20 @@ int main() {
     scanf("%f", &valor_compras);
 
     tempo_permanencia_seg = (hora_saida - hora_entrada) * 3600 + (min_saida - min_entrada) * 60 + (seg_saida - seg_entrada);
-
-    if (hora_entrada < 7 || hora_saida > 23) {
+    
+    if (hora_entrada < 7 || hora_saida > 23){
         printf("Estacionamento fechado! Volte entre 7h-23h.\n");
         return 0;
     }
 
-    if (tempo_permanencia_seg <= 3600) { // Se o tempo de permanência for de até 1 hora
-        custo_estacionamento = 12.0; // Cobrar o valor de 1 hora
-    } else if (tempo_permanencia_seg <= 7200) { // Se o tempo de permanência for de mais de 1 hora e até 2 horas
-        custo_estacionamento = 12.0; // Cobrar o valor de 1 hora
-    } else { // Se o tempo de permanência for superior a 2 horas
-        custo_estacionamento = ((tempo_permanencia_seg / 3600) + 1) * 12; // Arredonda para cima para cobrar a próxima hora
+    if (tempo_permanencia_seg <= 3600) {
+        valorEstacionamento = 12.00;
+    } else {
+        
+        horaPermanencia = (tempo_permanencia_seg + 3599) / 3600; 
+        valorEstacionamento = horaPermanencia * 12.00;
     }
 
-    // Aplica desconto com base no valor das compras
     if (valor_compras == 0.00) {
         desconto = 0.0;
     } else if (valor_compras <= 100) {
@@ -50,9 +55,10 @@ int main() {
         desconto = 1.0;
     }
 
-    custo_estacionamento = custo_estacionamento * (1.0 - desconto);
+    valorEstacionamento = valorEstacionamento * (1.0 - desconto);
 
-    printf("Custo do estacionamento: R$ %.2f\n", custo_estacionamento);
+    printf("Custo do estacionamento: R$ %.2f\n", valorEstacionamento);
 
     return 0;
+
 }
