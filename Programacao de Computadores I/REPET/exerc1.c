ccfa28d1b7a0642bc75ea267b5925d1f5b5ab3fc
Rafaela <rafaela.mumbach@acad.ufsm.br>
2024-05-06 17:52:18 -0300

    ok

--- Programacao de Computadores I/REPET/exerc1.c
+++ Programacao de Computadores I/REPET/exerc1.c
@@ -5,13 +5,10 @@
 */
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(){
-	float val1, val2;
-    printf("Digite dois valores: ");
-    scanf("%f %f", &val1, &val2);
 
-    printf("A media aritmetica dos dois valores eh: %.2f", (val1+val2)/2);
-    
+    system("PAUSE");
     return 0;
 }    
