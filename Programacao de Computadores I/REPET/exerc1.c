39a0baf39066d794803d7a7fbd6b4f7d994a736b
Rafaela <rafaela.mumbach@acad.ufsm.br>
2024-05-06 17:51:46 -0300

    ok

---
+++ Programacao de Computadores I/REPET/exerc1.c
@@ -0,0 +1,17 @@
+/* ---- Exercicios de aprendizagem
+   ---- Comandos de repeticao
+   ---- Rafaela Mumbach Buenos
+   ---- V01 - 06/05/2024
+*/
+
+#include <stdio.h>
+
+int main(){
+	float val1, val2;
+    printf("Digite dois valores: ");
+    scanf("%f %f", &val1, &val2);
+
+    printf("A media aritmetica dos dois valores eh: %.2f", (val1+val2)/2);
+    
+    return 0;
+}    
