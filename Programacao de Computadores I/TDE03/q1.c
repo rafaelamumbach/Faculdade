a6956be79c81d8a5d31771978d78ced8044bcce4
Rafaela <rafaela.mumbach@acad.ufsm.br>
2024-05-21 16:11:23 -0300

    ok

--- Programacao de Computadores I/TDE03/q1.c
+++ Programacao de Computadores I/TDE03/q1.c
@@ -1,11 +1,15 @@
 /* ---- TDE 02
    ---- (Q1) Provas Anteriores     
    ---- Rafaela Mumbach Buenos
-   ---- V03 - 04/04/2024
+   ---- V01 - 14/05/2024
+   ----
+   Faça um programa em C que mostre todos os números entre 0 e 100 cuja soma dos dígitos seja um número múltiplo de 3. Mostre o número seguido da soma de seus dígitos.
+   -----
 */
 
 #include <stdio.h>
 
 int main() {
+    
 
 }
