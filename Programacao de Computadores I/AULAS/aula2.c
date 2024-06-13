b087e24a086f143b45d54f29e7745c9e3c9271d5
Rafaela <rafaela.mumbach@acad.ufsm.br>
2024-06-13 19:10:07 -0300

    ok

--- Programacao de Computadores I/AULAS/aula2.c
+++ Programacao de Computadores I/AULAS/aula2.c
@@ -1,12 +1,27 @@
 /* ---- Estruturas de Repeticao
-   ---- Exercicio 02   
+   ---- Exercicio 01   
    ---- Rafaela Mumbach Buenos
    ---- V01 - 13/06/2024
 */
 
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    
+int main(){
+    int min=0,max=100;
+    int n, soma;
 
+    printf("\n\nNumeros cujas somas dos digitos sao divisiveis por 3 (%d,%d):\n",min,max);
+
+    for(n = min; n <= max; n++){
+        soma = (n/10)+(n%10);
+        if(soma / 3 != 0){
+            if(soma%3 == 0){
+                printf(" %02d\tSoma Digitos: %d\n",n,soma);
+            }
+        }
+    }
+    printf("\n\n");
+    system("PAUSE");
+    return 0;
 }
